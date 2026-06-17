364db8308c9a62fe8c00a7af4ae370a5999a4f85
Sparyn <Sparyn7@gmail.com>
2026-06-17 16:28:57 +0800

    :bug: fix(nvim): manage gopls exclusively with mason

--- lua/plugins/go.nvim.lua
+++ lua/plugins/go.nvim.lua
@@ -7,11 +7,7 @@ return {
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
-  build = function() -- 修正构建命令
-    require("go.install").update_all_sync()
-  end,
   config = function()
     require("go").setup({
       verbose = false,
@@ -24,19 +20,6 @@ return {
         update_in_insert = false,
       },
       dap_debug = false,
-    })
-    -- require("go.format").gofmt()  -- 只使用 gofmt
-    require("go.format").goimports() -- goimports + gofmt
-
-    -- 自动格式化配置 (修正缩进和语法)
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      group = format_sync_grp,
-      callback = function()
-        -- require("go.format").gofmt()
-        require("go.format").goimports() -- goimports + gofmt
-      end,
     })
   end,
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,13 @@
 -- https://github.com/mason-org/mason.nvim
 -- mason 用于管理、下载 lsp
 return {
-  "mason-org/mason.nvim",
-  opts = {},
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+  opts = {
+    ensure_installed = { "gopls" },
+    automatic_enable = false,
+  },
 }
