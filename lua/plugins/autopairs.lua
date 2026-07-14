63deb9db35f3429fa872b980f78676dc433b6622
Sparyn <Sparyn7@gmail.com>
2026-07-15 00:35:04 +0800

    :sparkles: feat(autopairs): enable treesitter-aware pairing

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,24 @@
 -- https://github.com/windwp/nvim-autopairs
--- 自动补全括号
+-- 自动补全并维护括号、引号等成对字符。
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  config = true,
-  -- use opts = {} for passing setup options
-  -- this is equivalent to setup({}) function
+  opts = {
+    -- 借助 Treesitter 判断语法上下文，减少字符串等位置的错误配对。
+    check_ts = true,
+
+    -- 在插件输入框和特殊 Buffer 中禁用自动配对。
+    disable_filetype = {
+      "TelescopePrompt",
+      "spectre_panel",
+      "snacks_picker_input",
+    },
+
+    -- 同一行已经存在右括号时，不再重复生成。
+    enable_check_bracket_line = true,
+
+    -- 回车时展开括号，退格时成对删除。
+    map_cr = true,
+    map_bs = true,
+  },
 }
