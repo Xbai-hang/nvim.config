76b07354a59c0fddcd39a0d5edbcd65c87f649ce
Sparyn <Sparyn7@gmail.com>
2026-07-16 01:51:37 +0800

    :sparkles: feat(mason): ensure core language servers

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,13 @@
 -- https://github.com/mason-org/mason.nvim
--- mason 用于管理、下载 lsp
+-- 安装和管理 Neovim 使用的 LSP、格式化器等外部工具。
+--
+-- 操作：
+--   :Mason              打开工具管理界面
+--   :MasonInstall <pkg> 安装工具
+--   :MasonUpdate        更新工具仓库
+--   :MasonLog           查看安装日志
+--
+-- NOTE: 网络失败不会影响 Neovim 启动；恢复后重新安装即可。
 return {
   "mason-org/mason-lspconfig.nvim",
   dependencies = {
@@ -7,7 +15,12 @@ return {
     "neovim/nvim-lspconfig",
   },
   opts = {
-    ensure_installed = { "gopls" },
+    ensure_installed = {
+      "gopls",
+      "lua_ls",
+      "ty",
+    },
+    -- Mason 只负责安装，启用列表由 vim.lsp.enable() 统一维护。
     automatic_enable = false,
   },
 }
