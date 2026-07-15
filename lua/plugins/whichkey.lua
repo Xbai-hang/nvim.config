bb657d875179e360379e0b8c1346f52115a5c2fc
Sparyn <Sparyn7@gmail.com>
2026-07-16 02:23:39 +0800

    :sparkles: feat(which-key): organize shortcut groups

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,12 +1,23 @@
 -- https://github.com/folke/which-key.nvim
--- 快速查看快捷键，解放双脑
+-- 显示当前可用的快捷键及其功能分组。
+--
+-- 操作：
+--   <leader>? 查看当前 Buffer 可用的快捷键
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    spec = {
+      { "<leader>b", group = "Buffer 操作" },
+      { "<leader>f", group = "查找与格式化" },
+      { "<leader>fc", group = "查找注释标记" },
+      { "<leader>fz", group = "模糊查找（FzfLua）" },
+      { "<leader>g", group = "Git 操作" },
+      { "<leader>gh", group = "Git 变更块（Hunk）" },
+      { "<leader>n", group = "通知（Notifications）" },
+      { "<leader>ot", group = "终端（Terminal）" },
+      { "<leader>u", group = "功能开关" },
+    },
   },
   keys = {
     {
@@ -14,7 +25,7 @@ return {
       function()
         require("which-key").show({ global = false })
       end,
-      desc = "Buffer Local Keymaps (which-key)",
+      desc = "查看当前 Buffer 快捷键",
     },
   },
 }
